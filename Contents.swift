633efdd44efff4557ce92eccc171a78dd577d089
user185916 <user185916@74-80-250-245.ic2net.net>
2021-02-17 13:45:59 -0800

    Actividad

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Punto 4
+var entero = 1
+var decimal:Float = 8.7
+var texto:String = String()
+var TF = true
+
+// Punto 5
+let texto2:String = "Hola a todos"
+let entero2:Int = 10
+
+// Punto 6
+var vArray = [1,2,3,4,5,6,7,8,9,10]
+var vDic = [1:"Domingo", 2:"Lunes", 3:"Martes", 4:"Miercoles", 5:"Jueves", 6:"Viernes", 7:"Sabado"]
